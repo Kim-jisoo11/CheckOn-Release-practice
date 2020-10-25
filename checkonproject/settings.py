2016bc8b06ec20ecc68fc2a4d606a0b41de7883e
김지수 <kingwltn@naver.com>
2020-10-25 20:04:48 +0900

    settings.py

--- checkonproject/settings.py
+++ checkonproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*cifkyo%fv)(%ol4s6ydc4*qm=ted)t@j7)p)uu*d+4#ws=%gc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sungshin2dx2-env.eba-zstah2bh.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
